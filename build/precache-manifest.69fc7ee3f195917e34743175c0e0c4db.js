c63e7c654f875aa40fbe16406b3739b13d86f6cf
shah.neell <shah.neell@husky.neu.edu>
2020-12-06 17:27:17 -0500

    testing

---
+++ build/precache-manifest.69fc7ee3f195917e34743175c0e0c4db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cd34f6b633d3268cd1992ad0f9c243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bdc4790a08382c699e5",
+    "url": "/static/css/main.d3f3977a.chunk.css"
+  },
+  {
+    "revision": "d76fe918e3e504c35130",
+    "url": "/static/js/2.d3643521.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3643521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdc4790a08382c699e5",
+    "url": "/static/js/main.74dd9588.chunk.js"
+  },
+  {
+    "revision": "cb7abcf8d3088f151358",
+    "url": "/static/js/runtime-main.df7f9a4d.js"
+  },
+  {
+    "revision": "a50cf9141094f644c04e5ac53cec7330",
+    "url": "/static/media/spinner.a50cf914.svg"
+  },
+  {
+    "revision": "07259c5ab4fb3dfcec1be7ae3b2a18f4",
+    "url": "/static/media/unavailable.07259c5a.jpg"
+  }
+]);
